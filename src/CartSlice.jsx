c4207ce3c6cb5b0d392831b2ada70a48d1dbc87a
charlene-m24 <gmqiti@gmail.com>
2024-11-28 03:14:48 -0500

    task 1 to 3 completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
+        const itemUpToDate = state.items.find(item=> item.name === action.payload.name);
+        if (itemUpToDate){
+            item.quantity = action.payload.quantity;
+        }
     
     },
   },
